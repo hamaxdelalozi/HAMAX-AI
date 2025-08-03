7d895db1ceed00b73d5a99e04e54ac2d40ea8c60
hamaxdelalozi <hamaxdelalozi@gmail.com>
2025-08-03 09:18:31 +0300

    Update set.js

--- set.js
+++ set.js
@@ -14,7 +14,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "254793753327",
+    OWNER_NAME: process.env.OWNER_NAME || "254735921879",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " AndalaTheMr",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
